7fcadc0de656658425b1c94b8aee1e1213cb2dba
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-12 12:51:54 +0200

    add actual values

--- db/migrate/20160812103252_create_cards.rb
+++ db/migrate/20160812103252_create_cards.rb
@@ -1,6 +1,10 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
+      t.string :username, null: false
+      t.string :organization, null: false
+      t.string :privacy, null: false, defualt: 'private'
+      t.uuid :visualization_id, null: false
     end
   end
 end

6a02563108aab614f4ec30836b1680377b3457d7
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-12 14:16:56 +0200

    allow nil orgs

--- db/migrate/20160812103252_create_cards.rb
+++ db/migrate/20160812103252_create_cards.rb
@@ -2,7 +2,7 @@ class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
       t.string :username, null: false
-      t.string :organization, null: false
+      t.string :organization
       t.string :privacy, null: false, defualt: 'private'
       t.uuid :visualization_id, null: false, unique: true
       t.timestamps

8e41e1e8f0b3929658062b0c3c79b939a5a1ccac
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-15 14:51:51 +0200

    rename key

--- db/migrate/20160815105547_create_apps.rb
+++ db/migrate/20160815105547_create_apps.rb
@@ -2,7 +2,8 @@ class CreateApps < ActiveRecord::Migration[5.0]
   def change
     create_table :apps do |t|
       t.string :name, null: false
-      t.string :token, null: false
+      t.string :app_id, null: false
+      t.string :app_key, null: false
 
       t.timestamps
     end

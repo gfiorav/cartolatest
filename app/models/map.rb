f1cd5cd7015a3350c77477e7b6e5eb67a82c2582
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-15 14:43:34 +0200

    add map title

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,12 +1,13 @@
 class Map < ActiveRecord::Base
-  validates :username, :visualization_id, :privacy, presence: true
+  validates :author, :visualization_id, :privacy, presence: true
+  validates :name
   validates :visualization_id, uniqueness: true
   validates :privacy, inclusion: { in: ['public'] } # don't load link/private vis
 
   def self.from_event(event)
     indifferent_event = event.with_indifferent_access
 
-    new(username: indifferent_event[:username],
+    new(author: indifferent_event[:username],
         organization: indifferent_event[:organization],
         visualization_id: indifferent_event[:vis_id],
         privacy: indifferent_event[:privacy],
@@ -18,14 +19,14 @@ class Map < ActiveRecord::Base
   end
 
   def embed_url
-    subdomain = organization.present? ? organization : username
+    subdomain = organization.present? ? organization : author
 
-    "https://#{subdomain}.carto.com/u/#{username}/builder/#{visualization_id}/embed"
+    "https://#{subdomain}.carto.com/u/#{author}/builder/#{visualization_id}/embed"
   end
 
   def url
     template_name = "tpl_#{visualization_id.gsub('-','_')}"
 
-    "https://#{username}.carto.com/api/v1/map/static/named/#{template_name}/{X}/{Y}.png"
+    "https://#{author}.carto.com/api/v1/map/static/named/#{template_name}/{X}/{Y}.png"
   end
 end

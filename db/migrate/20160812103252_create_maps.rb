2338b521f1c4199dbc584ada7441f3a85ebe5241
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-15 14:47:24 +0200

    author -> title

--- db/migrate/20160812103252_create_maps.rb
+++ db/migrate/20160812103252_create_maps.rb
@@ -1,7 +1,8 @@
 class CreateMaps < ActiveRecord::Migration[5.0]
   def change
     create_table :maps do |t|
-      t.string :username, null: false
+      t.string :author, null: false
+      t.string :title, null: false, default: 'Untitled Map'
       t.string :organization
       t.string :privacy, null: false, defualt: 'private'
       t.uuid :visualization_id, null: false, unique: true

de791c761bb39bd011854a51dd7c2651322a3faf
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-08-15 14:52:09 +0200

    rename mod els

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,9 +1,10 @@
 class App < ApplicationRecord
-  has_secure_token
+  has_secure_token :app_id
+  has_secure_token :app_key
 
   validates :name, length: { in: 4..24 }
 
-  def authorized?(token)
-    self.token == token
+  def authorized?(id, key)
+    app_id == id && app_key == key
   end
 end
